a098ffd925ec7d9a327947b7fe2df19dda73a3ff
Marcos Vinicius Batista Souza <mv.batista2022@gmail.com>
2023-01-06 13:38:15 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const Buttonplay = document.querySelector('.play')
+const Buttonpause = document.querySelector('.pause')
+const ButtonsoundOn = document.querySelector('.sound-on')
+const ButtonsoundOff = document.querySelector('.sound-off')
+const Buttonstop = document.querySelector('.stop')
+const Buttonset = document.querySelector('.set')
+
+
+Buttonplay.addEventListener('click', function(){
+    Buttonplay.classList.add('hide')
+    Buttonpause.classList.remove('hide')
+    Buttonset.classList.add('hide')
+    Buttonstop.classList.remove('hide')
+})
+
+Buttonpause.addEventListener('click', function(){
+    Buttonplay.classList.remove('hide')
+    Buttonpause.classList.add('hide')
+})
+
+Buttonstop.addEventListener('click', function(){
+    Buttonstop.classList.add('hide')
+    Buttonset.classList.remove('hide')
+    Buttonpause.classList.add('hide')
+    Buttonplay.classList.remove('hide')
+})
+
+ButtonsoundOn.addEventListener('click', sound())
+ButtonsoundOff.addEventListener('click', sound())
+
+function sound(){
+    ButtonsoundOn.classList.toggle('hide')
+    ButtonsoundOff.classList.toggle('hide')
+    return sound
+}
